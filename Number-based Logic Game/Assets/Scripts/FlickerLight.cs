d00b9878151d2c760f1db3bc292fa0f7caa15d2e
Jonasz Kadziela <jonaszkadziela@o2.pl>
2019-10-06 01:02:17 +0200

    Added Fog effect & Point light

---
+++ Number-based Logic Game/Assets/Scripts/FlickerLight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class FlickerLight : MonoBehaviour
+{
+    public float flickerSpeed = 0.2f;
+    public float multiplyScale = 1.05f;
+
+    private Vector3 initialScale;
+    private Vector3 targetScale;
+
+    private bool increasing = true;
+
+    void Start()
+    {
+        initialScale = transform.localScale;
+    }
+
+    void Update()
+    {
+        targetScale = increasing ? initialScale * multiplyScale : initialScale;
+        transform.localScale = Vector3.Lerp(transform.localScale, targetScale, flickerSpeed);
+
+        if (Vector3.Distance(transform.localScale, targetScale) < 0.01f)
+        {
+            increasing = !increasing;
+        }
+    }
+}
---
+++ Number-based Logic Game/Assets/Scripts/FogManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class FogManager : MonoBehaviour
+{
+    public Tilemap darkOverlay;
+    public Tilemap transparentOverlay;
+    public Tilemap groundTilemap;
+
+    public Tile darkTile;
+    public Tile transparentTile;
+
+    void Start()
+    {
+        darkOverlay.origin = groundTilemap.origin;
+        darkOverlay.size = groundTilemap.size;
+
+        transparentOverlay.origin = groundTilemap.origin;
+        transparentOverlay.size = groundTilemap.size;
+
+        foreach (Vector3Int pos in darkOverlay.cellBounds.allPositionsWithin)
+        {
+            darkOverlay.SetTile(pos, darkTile);
+        }
+        foreach (Vector3Int pos in darkOverlay.cellBounds.allPositionsWithin)
+        {
+            transparentOverlay.SetTile(pos, transparentTile);
+        }
+    }
+}
