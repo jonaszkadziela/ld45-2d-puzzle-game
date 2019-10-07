f8a6435521f7ad8c5628dd52b56ad87165b0f66c
Jonasz Kadziela <jonaszkadziela@o2.pl>
2019-10-08 01:58:31 +0200

    Added GameOver UI

---
+++ Number-based Logic Game/Assets/Scripts/GameOverUI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class GameOverUI : MonoBehaviour
+{
+    public GameObject gameOverUI;
+    public GameObject statisticsUI;
+
+    public TextMeshProUGUI roundsCompletedValue;
+
+    void Update()
+    {
+        if (GameManager.GameOver)
+        {
+            if (!gameOverUI.activeSelf)
+            {
+                statisticsUI.SetActive(false);
+                gameOverUI.SetActive(true);
+
+                roundsCompletedValue.text = GameplayManager.CurrentRound.ToString();
+            }
+        }
+        else
+        {
+            statisticsUI.SetActive(true);
+            gameOverUI.SetActive(false);
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Restart()
+    {
+
+    }
+}
