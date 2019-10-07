70a0b260d02e716c06f53c2a83e67d8841602fd9
Jonasz Kadziela <jonaszkadziela@o2.pl>
2019-10-07 22:21:57 +0200

    Added AudioManager script

---
+++ Number-based Logic Game/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    public AudioMixer audioMixer;
+    public AudioMixerGroup masterGroup;
+
+    public float fadeDuration = 1f;
+    public AnimationCurve fadeCurve;
+
+    void Awake()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        AdjustMixerVolumes();
+        audioMixer.SetFloat("MasterVolume", RemapVolume(0f));
+    }
+
+    public static float RemapVolume(float value01)
+    {
+        return Mathf.Lerp(-80f, 0f, value01);
+    }
+
+    public void AdjustMixerVolumes()
+    {
+        audioMixer.SetFloat("MasterVolume", RemapVolume(Settings.MasterVolume));
+    }
+
+    public void Mute()
+    {
+        StartCoroutine(FadeOut());
+    }
+
+    public void Unmute()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    private IEnumerator FadeIn()
+    {
+        float time = 0f;
+
+        while (time < fadeDuration)
+        {
+            time += Time.unscaledDeltaTime;
+            float volume = fadeCurve.Evaluate(time / fadeDuration);
+
+            audioMixer.SetFloat("MasterVolume", RemapVolume(Mathf.Clamp(volume, 0f, Settings.MasterVolume)));
+
+            yield return 0;
+        }
+    }
+
+    private IEnumerator FadeOut()
+    {
+        float time = fadeDuration;
+
+        while (time > 0f)
+        {
+            time += Time.unscaledDeltaTime;
+            float volume = fadeCurve.Evaluate(time / fadeDuration);
+
+            audioMixer.SetFloat("MasterVolume", RemapVolume(Mathf.Clamp(volume, 0f, Settings.MasterVolume)));
+
+            yield return 0;
+        }
+    }
+}
---
+++ Number-based Logic Game/Assets/Scripts/Settings.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    /**
+     * Public variables to set values in UnityEditor
+     */
+
+    [Header("Audio Settings")]
+    public float masterVolume = 1f;
+
+    /**
+     * Public static variables to use them easily in other scripts
+     */
+
+    public static float MasterVolume;
+
+    void Awake()
+    {
+        MasterVolume = masterVolume;
+    }
+}
